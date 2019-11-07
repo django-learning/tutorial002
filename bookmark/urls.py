d8097f9f88e545ae725edef169dd228d5b3991f9
cslee-dev <dev.cslee@gmail.com>
2019-11-07 09:28:36 +0900

    [add] 북마크 디테일, 추가

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import bookmark_list_view
+from .views import bookmark_list_view, bookmark_create_view, bookmark_detail_view
 app_name = "bookmark"
 
 urlpatterns = [
-    path("", bookmark_list_view, name="index")
+    path("", bookmark_list_view, name="index"),
+    path("create/", bookmark_create_view, name="create"),
+    path("<int:pk>/bookmark/", bookmark_detail_view, name="detail"),
 ]
